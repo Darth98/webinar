188e4c46b9fbf12ac638216759791f8fd3ad4224
Darth98 <whalyssonaguiar@hotmail.com>
2020-10-22 19:26:27 -0300

    correcao dao

--- AulaColecoe/src/webinar/dao/InstrutorDao.java
+++ AulaColecoe/src/webinar/dao/InstrutorDao.java
@@ -10,50 +10,43 @@ import javax.persistence.Query;
 import webinar.model.Formacao;
 import webinar.model.Instrutor;
 
-public class InstrutorDao implements Dao<Instrutor>{
+public class InstrutorDao implements Dao<Instrutor> {
 
 	private EntityManager em;
-	
+		
 	public InstrutorDao() {
 		em = JPAUtil.getEntityManagerFactory().createEntityManager();
 	}
-	
+
 	@Override
 	public Instrutor get(int id) {
-		
-		return  em.find(Instrutor.class, id);
+		return em.find(Instrutor.class, id);
 	}
 
-	
-	public List<Instrutor> getAllbyFormacao(Formacao pformacao) {
-	
-		Query q  = em.createQuery("SELECT i FROM Instrutor i WHERE i.formacao = :pformacao ");
-		q.setParameter("pformacao",pformacao);
-		return q.getResultList();
-	}
-	
 	@Override
 	public List<Instrutor> getAll() {
-		return em.createQuery("FROM Instrutor", Instrutor.class).getResultList();
+		return em.createQuery("FROM_Instrutor", Instrutor.class).getResultList();
+	}
+	
+	public List<Instrutor> getAllByFormacao(Formacao formacao){
+		Query q = em.createQuery("SELECT i FROM Instrutor i WHERE i.formacao = :formacao");
+		q.setParameter("formacao", formacao);
+		return q.getResultList();
 	}
 
 	@Override
 	public void save(Instrutor t) {
-		
-		executeInsideTransaction(em->em.persist(t));
-		
+		executeInsideTransaction(em -> em.persist(t));	
 	}
 
 	@Override
 	public void update(Instrutor t) {
-		// TODO Auto-generated method stub
-		
+		executeInsideTransaction(em -> em.merge(t));		
 	}
 
 	@Override
 	public void delete(Instrutor t) {
-		// TODO Auto-generated method stub
-		
+		executeInsideTransaction(em -> em.remove(t));		
 	}
 	
 	private void executeInsideTransaction(Consumer<EntityManager> action) {
@@ -62,7 +55,8 @@ public class InstrutorDao implements Dao<Instrutor>{
 			tx.begin();
 			action.accept(em);
 			tx.commit();
-		} catch (RuntimeException e) {
+		}
+		catch(RuntimeException e) {
 			tx.rollback();
 			throw e;
 		}
